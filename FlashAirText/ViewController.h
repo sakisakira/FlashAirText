abe591f07ea8d5bcb3ed122d0916b16ca214ecb3
SAkira <sakira@sakira.jp>
2014-04-25 23:03:57 +0900

    Initial Commit

---
+++ FlashAirText/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlashAirText
+//
+//  Created by sakira on 2014/04/25.
+//  Copyright (c) 2014年 sakira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
